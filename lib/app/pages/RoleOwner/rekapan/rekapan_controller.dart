cbcf50512fdf20eade1e2a9e6d70785f1c7ddd45
aryadinata07 <aryadinata0778@gmail.com>
2024-05-04 11:05:54 +0700

    feat: [AR] Add page rekapan pemesanan

--- lib/app/pages/RoleOwner/rekapan/rekapan_controller.dart
+++ lib/app/pages/RoleOwner/rekapan/rekapan_controller.dart
@@ -1,6 +1,72 @@
 import 'package:get/get.dart';
 
 class RekapanController extends GetxController {
+  var rekapan = [
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+    {
+      'name': 'Nasi Goreng Beef',
+      'item': '3',
+      'kode': 'M-JN024',
+      'total': 'Rp 51.000',
+      'pembayaran': "Tunai",
+      'tempat': "makan di tempat",
+      'jumlahPesanan': '5',
+    },
+  ];
+
   @override
   void onInit() {
     super.onInit();
--- lib/app/pages/RoleOwner/rekapan/rekapan_view.dart
+++ lib/app/pages/RoleOwner/rekapan/rekapan_view.dart
@@ -1,10 +1,14 @@
 import 'package:angkringan_omaci_ta/app/global_components/appbar_owner.dart';
 import 'package:angkringan_omaci_ta/app/global_components/background.dart';
+import 'package:angkringan_omaci_ta/app/global_components/rekapan_menu.dart';
 import 'package:angkringan_omaci_ta/app/global_components/sidebar/sidebar_view.dart';
 import 'package:angkringan_omaci_ta/app/pages/RoleOwner/rekapan/rekapan_controller.dart';
 import 'package:angkringan_omaci_ta/common/helper/themes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RekapanView extends StatelessWidget {
   const RekapanView({super.key});
@@ -18,67 +22,103 @@ class RekapanView extends StatelessWidget {
         child: GetBuilder<RekapanController>(
           builder: (controller) {
             return SafeArea(
-              child: Column(
+              child: ListView(
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CustomAppBar(context: context, title: 'Rekapan Pemesanan',),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                    child: SizedBox(
-                                      height: 60,
-                                      child: TextField(
-                                        decoration: InputDecoration(
-                                          hintText: 'Cari Menu',
-                                          hintStyle: const TextStyle(
-                                            color: grey,
-                                          ),
-                                          filled: true,
-                                          fillColor: textFieldBackground,
-                                          border: OutlineInputBorder(
-                                            borderRadius: BorderRadius.circular(8.0),
-                                            borderSide: BorderSide.none,
-                                          ),
-                                          prefixIcon: const Icon(
-                                            Icons.search,
-                                            color: grey,
-                                          ),
-                                        ),
-                                        style: const TextStyle(
-                                          color: grey,  // Set the color of the entered value to grey
-                                        ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomAppBar(
+                          context: context,
+                          title: 'Rekapan Pemesanan',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 60,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: '01 Mar - 10 Mar 2024',
+                                      hintStyle: const TextStyle(
+                                        color: grey,
                                       ),
-                                    )
-
-                                ),
-                                const SizedBox(width: 10.0),
-                                Container(
-                                  height: 60,
-                                  width: 60,
-                                  decoration: BoxDecoration(
-                                    color: primaryAccent,
-                                    borderRadius: BorderRadius.circular(8.0),
+                                      filled: true,
+                                      fillColor: textFieldBackground,
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                        borderSide: BorderSide.none,
+                                      ),
+                                      prefixIcon: const Icon(
+                                        Icons.calendar_today,
+                                        color: grey,
+                                        size: 20,
+                                      ),
+                                      suffixIcon: IconButton(
+                                        icon: const Icon(
+                                            Icons.expand_more_outlined),
+                                        iconSize: 30,
+                                        color: white,
+                                        onPressed: () {},
+                                      )),
+                                  style: const TextStyle(
+                                    color: grey,
                                   ),
-                                  child: IconButton(
-                                    icon: const Icon(
-                                      Icons.filter_list,
-                                      color: white,
+                                ),
+                              ),
+                              const SizedBox(height: 10.0),
+                              SizedBox(
+                                height: 60,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Cari rekapan pesanan',
+                                    hintStyle: const TextStyle(
+                                      color: grey,
                                     ),
-                                    onPressed: () {},
+                                    filled: true,
+                                    fillColor: textFieldBackground,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      borderSide: BorderSide.none,
+                                    ),
+                                    prefixIcon: const Icon(
+                                      Icons.search,
+                                      color: grey,
+                                    ),
+                                  ),
+                                  style: const TextStyle(
+                                    color: grey,
                                   ),
                                 ),
-                              ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: SizedBox(
+                            height: MediaQuery.of(context).size.height - 200,
+                            child: ListView.builder(
+                              itemCount: controller.rekapan.length,
+                              itemBuilder: (context, index) {
+                                final menu = controller.rekapan[index];
+                                return RekapanCard(
+                                  nama: menu['name']!,
+                                  item: menu['item']!,
+                                  kodePesanan: menu['kode']!,
+                                  totalPesanan: menu['total']!,
+                                  pembayaran: menu['pembayaran']!,
+                                  tempatMakan: menu['tempat']!,
+                                  jumlahPesnan: menu['jumlahPesanan']!,
+                                );
+                              },
                             ),
                           ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
                   ),
                 ],
